75eb5fda6deccdace003f56a71579f4bf525353a
Björn Kjeholt <dev@kjeholt.se>
2017-02-02 07:43:41 +0100

    Update mqttBasics.js

--- js/Classes/support/mqttBasics.js
+++ js/Classes/support/mqttBasics.js
@@ -44,6 +44,46 @@ exports.topicStrToJson = function (str, callback) {
         }
     };
     
-exports.mqttPublish = function(mqttObj, nodeObj) {
-   
+exports.mqttPublish = function(mqttObj, nodeObj, configInfo) {        
+    var topicHeaderStr = { info_present: "info/present/" + self.ci.agent.name,
+                           data_request: "data/request/" + self.ci.agent.name };
+        
+    mqttObj.publish( topicHeaderStr.info_present,
+                     JSON.stringify({
+                                        time: Math.floor((new Date())/1000),
+                                        date: new Date(),
+                                        name: configInfo.agent.name,
+                                        rev: configInfo.agent.rev }),
+                     { qos: 0, retain: 1 });
+                                
+    nodeObj.getNodeInfo(null,function(err,topicJson,msgJson) {
+            var topicStr = "";
+            var msgStr = "";
+            
+            if (topicJson.order === "info_present")
+                topicStr = topicHeaderStr.info_present;
+            else
+                topicStr = topicHeaderStr.data_request;
+                
+            if (!err) {
+                if (topicJson.node !== undefined) {
+                    topicStr = topicStr + "/" + topicJson.node;
+                    msgStr = JSON.stringify(msgJson);
+                    
+                    if (topicJson.device !== undefined) {
+                        topicStr = topicStr + "/" + topicJson.device;
+                    
+                        if (topicJson.variable !== undefined) {
+                            // Create a variable message
+                            topicStr = topicStr + "/" + topicJson.variable;
+                    
+                        }
+                    }
+                                        
+                    mqttObj.publish(topicStr,msgStr,{ qos: 0, retain: 1 });
+                }
+            } else {
+                console.log("AgentBody: Error from getNodeInfo. err=",err);
+            }
+        });
 };
